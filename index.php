93479a1540aa6d8495db8d3cf279cd889e586ec3
Siam Hossain <siam.hossain1994@gmail.com>
2020-02-21 15:54:29 +0600

    Add Design

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+    require_once('process.php');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>php-crud</title>
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="row justify-content-center">
+        <form action="" method="POST">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" name="name" class="form-control" value="Enter your name">
+            </div>
+            <div class="form-group">
+                <label>Location</label>
+                <input type="text" name="location" class="form-control" value="Enter your location">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary" name="save">Save</button>
+            </div>
+        </form>
+    </div>
+    
+
+</body>
+</html>
